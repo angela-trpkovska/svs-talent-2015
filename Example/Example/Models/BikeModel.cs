27e14abc330e020dfc83d24e01d13409ab1396b5
angela-trpkovska <trpkovskaangela@gmail.com>
2015-04-22 17:03:26 +0200

    MVC project

---
+++ Example/Example/Models/BikeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.Models
+{
+    public class BikeModel
+    {
+        public string RegNumber { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public BikeModel(string RegNumber, string Producer, string Model, string Color)
+        {
+            this.RegNumber = RegNumber;
+            this.Producer = Producer;
+            this.Model = Model;
+            this.Color = Color;
+
+
+        }
+
+    }
+}
