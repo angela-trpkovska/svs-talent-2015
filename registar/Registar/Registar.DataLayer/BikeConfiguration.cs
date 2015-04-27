68723944b2fdc719a4e8a2a2b71c7ab5bac74067
angela-trpkovska <trpkovskaangela@gmail.com>
2015-04-27 11:39:11 +0200

    registar
    registar entity framework

---
+++ registar/Registar/Registar.DataLayer/BikeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Registar.DomainModel;
+
+namespace Registar.DataLayer
+{
+    public class BikeConfiguration : EntityTypeConfiguration<Bike>
+    {
+        public BikeConfiguration()
+        {
+            this.ToTable("Bikes", "Data")
+            //this.ToTable("Bikes")
+                
+                //.HasKey(x => new {Id = x.BikeId, Colour = x.Colour});
+                .HasKey(p => p.BikeId);
+
+            this.Property(p => p.BikeId)
+                .HasColumnName("Id");
+
+            this.Property(p => p.RegNumber)
+               .HasColumnName("RegNumber");
+
+            this.Property(p => p.Producer)
+               .HasColumnName("Producer");
+
+            this.Property(p => p.Model)
+               .HasColumnName("Model");
+            this.Property(p => p.Colour)
+               .HasColumnName("Color");
+
+            //this.Ignore(p => p.IgnoreMe);
+        }
+    }
+}
